e74c59072d7a738a1007ee198a5ecb1e6f748670
Aaron Campf <aaroncampf@gmail.com>
2016-08-14 20:58:57 -0700

    Added Code Test

--- NeoPotentialCustomer/NeoPotentialCustomer/Application.xaml.vb
+++ NeoPotentialCustomer/NeoPotentialCustomer/Application.xaml.vb
@@ -1,6 +1,14 @@
 ﻿Class Application
 
-    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
-    ' can be handled in this file.
+	' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+	' can be handled in this file.
+
+
+	Private Sub App_DispatcherUnhandledException(ByVal sender As Object, ByVal e As Windows.Threading.DispatcherUnhandledExceptionEventArgs)
+		'TODO: See if this works
+		MsgBox(e.Exception.ToString)
+	End Sub
+
+
 
 End Class
